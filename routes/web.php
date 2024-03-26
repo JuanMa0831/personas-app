22c228f4a739fedd0a56ae8ba415af3f1572fa73
Juan Manuel Valencia <3182003thebest@gmail.com>
2024-03-25 22:19:14 -0500

    Ajuste_grabar_nueva_comuna

--- routes/web.php
+++ routes/web.php
@@ -8,5 +8,6 @@ Route::get('/', function () {
     return view('welcome');
 });
 
-Route::get('/comunas', [ComunaController::class, 'index']);
-Route::get('/comunas/create', 'ComunaController@create')->name('comunas.create');
+Route::get('/comunas', [ComunaController::class, 'index'])->name('comunas.index');
+Route::get('/comunas/create', [ComunaController::class, 'create'])->name('comunas.create');
+Route::post('/comunas/store', [ComunaController::class, 'store'])->name('comunas.store');
